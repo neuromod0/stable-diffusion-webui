74e8ffb80d954b6bed4911a62804d81ae2eef621
Neuro Mod Agent <157213586+neuromod0@users.noreply.github.com>
2024-02-17 18:19:22 +0000

    utiks

--- components/shared.py
+++ components/shared.py
@@ -5,7 +5,7 @@ import gradio as gr
 from components import shared_cmd_options, shared_gradio_themes, options, shared_items
 from components.sd import sd_models_types
 from components.paths_internal import models_path, script_path, data_path, sd_configs_path, sd_default_config, sd_model_file, default_sd_model_file, extensions_dir, extensions_builtin_dir  # noqa: F401
-from components import util
+from utils import util
 
 cmd_opts = shared_cmd_options.cmd_opts
 parser = shared_cmd_options.parser
