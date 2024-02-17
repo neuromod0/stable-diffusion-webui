4edda31e636f90a2e7d7640db838bf329bdc8a9c
Neuro Mod Agent <157213586+neuromod0@users.noreply.github.com>
2024-02-17 18:12:31 +0000

    sd

--- components/shared_items.py
+++ components/shared_items.py
@@ -4,59 +4,59 @@ from components.shared_cmd_options import cmd_opts
 
 
 def realesrgan_models_names():
-    import modules.realesrgan_model
-    return [x.name for x in modules.realesrgan_model.get_realesrgan_models(None)]
+    import component.realesrgan_model
+    return [x.name for x in component.realesrgan_model.get_realesrgan_models(None)]
 
 
 def postprocessing_scripts():
-    import modules.scripts
+    import component.scripts
 
-    return modules.scripts.scripts_postproc.scripts
+    return component.scripts.scripts_postproc.scripts
 
 
 def sd_vae_items():
-    import modules.sd_vae
+    import component.sd_vae
 
-    return ["Automatic", "None"] + list(modules.sd_vae.vae_dict)
+    return ["Automatic", "None"] + list(component.sd_vae.vae_dict)
 
 
 def refresh_vae_list():
-    import modules.sd_vae
+    import component.sd_vae
 
-    modules.sd_vae.refresh_vae_list()
+    component.sd_vae.refresh_vae_list()
 
 
 def cross_attention_optimizations():
-    import modules.sd_hijack
+    import component.sd_hijack
 
-    return ["Automatic"] + [x.title() for x in modules.sd_hijack.optimizers] + ["None"]
+    return ["Automatic"] + [x.title() for x in component.sd_hijack.optimizers] + ["None"]
 
 
 def sd_unet_items():
-    import modules.sd_unet
+    import component.sd_unet
 
-    return ["Automatic"] + [x.label for x in modules.sd_unet.unet_options] + ["None"]
+    return ["Automatic"] + [x.label for x in component.sd_unet.unet_options] + ["None"]
 
 
 def refresh_unet_list():
-    import modules.sd_unet
+    import component.sd_unet
 
-    modules.sd_unet.list_unets()
+    component.sd_unet.list_unets()
 
 
 def list_checkpoint_tiles(use_short=False):
-    import modules.sd_models
-    return modules.sd_models.checkpoint_tiles(use_short)
+    import component.sd_models
+    return component.sd_models.checkpoint_tiles(use_short)
 
 
 def refresh_checkpoints():
-    import modules.sd_models
-    return modules.sd_models.list_models()
+    import component.sd_models
+    return component.sd_models.list_models()
 
 
 def list_samplers():
-    import modules.sd_samplers
-    return modules.sd_samplers.all_samplers
+    import component.sd_samplers
+    return component.sd_samplers.all_samplers
 
 
 def reload_hypernetworks():
@@ -113,7 +113,7 @@ def ui_reorder_categories():
     yield "scripts"
 
 
-class Shared(sys.modules[__name__].__class__):
+class Shared(sys.component[__name__].__class__):
     """
     this class is here to provide sd_model field as a property, so that it can be created and loaded on demand rather than
     at program startup.
@@ -123,15 +123,15 @@ class Shared(sys.modules[__name__].__class__):
 
     @property
     def sd_model(self):
-        import modules.sd_models
+        import component.sd_models
 
-        return modules.sd_models.model_data.get_sd_model()
+        return component.sd_models.model_data.get_sd_model()
 
     @sd_model.setter
     def sd_model(self, value):
-        import modules.sd_models
+        import component.sd_models
 
-        modules.sd_models.model_data.set_sd_model(value)
+        component.sd_models.model_data.set_sd_model(value)
 
 
-sys.modules['modules.shared'].__class__ = Shared
+sys.component['modules.shared'].__class__ = Shared
