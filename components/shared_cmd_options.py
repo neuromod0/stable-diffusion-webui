185c45b56d3f32c93a278d2681a21949697bbba9
Neuro Mod Agent <157213586+neuromod0@users.noreply.github.com>
2024-02-17 18:04:47 +0000

    utils

--- components/shared_cmd_options.py
+++ components/shared_cmd_options.py
@@ -1,7 +1,8 @@
 import os
 
 import launch
-from utils import cmd_args, script_loading
+from utils import cmd_args
+from components import script_loading
 from components.paths_internal import models_path, script_path, data_path, sd_configs_path, sd_default_config, sd_model_file, default_sd_model_file, extensions_dir, extensions_builtin_dir  # noqa: F401
 
 parser = cmd_args.parser
